46fb626326a03f05b5ca9289c18396ba14170a26
kuroko9527 <kuroko9527@icloud.com>
2020-03-01 18:22:38 +0800

    "004 水仙花思路+1"

--- Python_DS/Practice004.py
+++ Python_DS/Practice004.py
@@ -10,12 +10,21 @@ for i in range(100, 1000):
     if h == i:
         print(i)
 
+# 水仙花的第二种思路 #
+for i in range(100, 1000):
+    temp = list(str(i))
+    a = int(temp[0])
+    b = int(temp[1])
+    c = int(temp[2])
+    if a ** 3 + b ** 3 + c ** 3 == i:
+        print(i)
+
 # 三色球问题
 # 红球 = 3 ，黄球 =3 ，蓝球 = 6， 从上面取8个球，编程计算摸出球的各种颜色搭配
 i = 1
 for r in range(0, 4):
     for y in range(0, 4):
-        for b in range(0, 7):
+        for b in range(0, 7):  # 绿球可以从2开始，因为绿球至少有两个
             if r + y + b == 8:
                 i += 1
                 print("颜色搭配方法{0}:红色{1},黄色{2},蓝色{3}".format(i, r, y, b))
